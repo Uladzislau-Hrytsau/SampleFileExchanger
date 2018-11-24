b53f523f610445885c7866f88e859546aa019bc2
vladikini45 <vladboy45@gmail.com>
2018-11-24 19:23:38 +0300

    Category, File, User

--- dao/src/main/java/com/exchange/test/dao/UserDao.java
+++ dao/src/main/java/com/exchange/test/dao/UserDao.java
@@ -1,4 +1,19 @@
 package com.exchange.test.dao;
 
+import org.springframework.dao.DataAccessException;
+
+import java.util.List;
+
 public interface UserDao {
+
+    /**
+     * Get all users list.
+     *
+     * @return all users list
+     * @throws DataAccessException
+     */
+    List<User> getAllUsers() throws DataAccessException;
+
+
+
 }

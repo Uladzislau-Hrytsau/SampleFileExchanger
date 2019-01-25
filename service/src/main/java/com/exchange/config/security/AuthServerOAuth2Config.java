c2027cd42da76283fa352202b273d497b46b0b31
vladikini45 <vladboy45@gmail.com>
2019-01-25 17:53:27 +0300

    authorization server works and we can get access token

--- service/src/main/java/com/exchange/config/security/AuthServerOAuth2Config.java
+++ service/src/main/java/com/exchange/config/security/AuthServerOAuth2Config.java
@@ -2,79 +2,135 @@ package com.exchange.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.approval.UserApprovalHandler;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-
-import javax.sql.DataSource;
 
 /**
  * The type Auth server o auth 2 config.
  */
+@EnableAuthorizationServer
 @Configuration
 @EnableWebSecurity
 public class AuthServerOAuth2Config
         extends AuthorizationServerConfigurerAdapter {
 
-    @Autowired
-    private DataSource dataSource;
 
+    /**
+     * The type Authorization server config.
+     */
 //    @Autowired
-//    private Environment env;
+//    @Qualifier("authenticationManagerBean")
+//    private AuthenticationManager authenticationManager;
+//
+//    @Override
+//    public void configure(
+//            AuthorizationServerSecurityConfigurer oauthServer)
+//            throws Exception {
+//        oauthServer
+//                .tokenKeyAccess("permitAll()")
+//                .checkTokenAccess("isAuthenticated()");
+//    }
+//
+//    @Override
+//    public void configure(ClientDetailsServiceConfigurer clients)
+//            throws Exception {
+//        clients.inMemory()
+//                .withClient("sampleClientId")
+//                .authorizedGrantTypes("implicit")
+//                .scopes("read")
+//                .autoApprove(true)
+//                .and()
+//                .withClient("clientIdPassword")
+//                .secret("secret")
+//                .authorizedGrantTypes(
+//                        "password", "authorization_code", "refresh_token")
+//                .scopes("read");
+//    }
+//
+//    @Override
+//    public void configure(
+//            AuthorizationServerEndpointsConfigurer endpoints)
+//            throws Exception {
+//
+//        endpoints
+//                .tokenStore(tokenStore())
+//                .authenticationManager(authenticationManager);
+//    }
+//
+//    /**
+//     * Token store token store.
+//     *
+//     * @return the token store
+//     */
+//    @Bean
+//    public TokenStore tokenStore() {
+//        return new InMemoryTokenStore();
+//    }
+//
+//    /**
+//     * Token service remote token services.
+//     *
+//     * @return the remote token services
+//     */
+//    @Primary
+//    @Bean
+//    public RemoteTokenServices tokenService() {
+//        RemoteTokenServices tokenService = new RemoteTokenServices();
+//        tokenService.setCheckTokenEndpointUrl(
+//                "http://localhost:8088/spring-security-oauth-server/oauth/check_token");
+//        tokenService.setClientId("fooClientIdPassword");
+//        tokenService.setClientSecret("secret");
+//        return tokenService;
+//    }
+    @Configuration
+    @EnableAuthorizationServer
+    public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    }
+
+    private static String REALM = "CRM_REALM";
+    private static final int ONE_DAY = 60 * 60 * 24;
+    private static final int THIRTY_DAYS = 60 * 60 * 24 * 30;
+
+    @Autowired
+    private TokenStore tokenStore;
+
+    @Autowired
+    private UserApprovalHandler userApprovalHandler;
 
     @Autowired
     @Qualifier("authenticationManagerBean")
     private AuthenticationManager authenticationManager;
 
     @Override
-    public void configure(
-            AuthorizationServerSecurityConfigurer oauthServer)
-            throws Exception {
-        oauthServer
-                .tokenKeyAccess("permitAll()")
-                .checkTokenAccess("isAuthenticated()");
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("crmClient1")
+                .secret("crmSuperSecret")
+                .authorizedGrantTypes("password", "refresh_token")
+                .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
+                .scopes("read", "write", "trust")
+                //.accessTokenValiditySeconds(ONE_DAY)
+                .accessTokenValiditySeconds(300)
+                .refreshTokenValiditySeconds(THIRTY_DAYS);
     }
 
     @Override
-    public void configure(ClientDetailsServiceConfigurer clients)
-            throws Exception {
-        clients.jdbc(dataSource)
-                .withClient("sampleClientId")
-                .authorizedGrantTypes("implicit")
-                .scopes("read")
-                .autoApprove(true)
-                .and()
-                .withClient("clientIdPassword")
-                .secret("secret")
-                .authorizedGrantTypes(
-                        "password", "authorization_code", "refresh_token")
-                .scopes("read");
-    }
-
-    @Override
-    public void configure(
-            AuthorizationServerEndpointsConfigurer endpoints)
-            throws Exception {
-
-        endpoints
-                .tokenStore(tokenStore())
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(tokenStore).userApprovalHandler(userApprovalHandler)
                 .authenticationManager(authenticationManager);
     }
 
-    /**
-     * Token store token store.
-     *
-     * @return the token store
-     */
-    @Bean
-    public TokenStore tokenStore() {
-        return new JdbcTokenStore(dataSource);
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+        oauthServer.realm(REALM);
     }
 }
--- service/src/main/java/com/exchange/config/security/ServerSecurityConfig.java
+++ service/src/main/java/com/exchange/config/security/ServerSecurityConfig.java
@@ -1,56 +1,150 @@
 package com.exchange.config.security;
 
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * The type Server security config.
+ */
+//@EnableWebSecurity
+//@Configuration
+//public class ServerSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth)
+//            throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("l").password("p").roles("USER");
+//    }
+//
+//    @Override
+//    @Bean
+//    public AuthenticationManager authenticationManagerBean()
+//            throws Exception {
+//        return super.authenticationManagerBean();
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/login").permitAll()
+//                .antMatchers("/oauth/token").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin().permitAll();
+//    }
+//}
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
+import org.springframework.security.oauth2.provider.approval.TokenStoreUserApprovalHandler;
+import org.springframework.security.oauth2.provider.request.DefaultOAuth2RequestFactory;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 /**
  * The type Server security config.
  */
-@EnableWebSecurity
 @Configuration
+@EnableWebSecurity
 public class ServerSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth)
-            throws Exception {
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
+    /**
+     * Global user details.
+     *
+     * @param auth the auth
+     * @throws Exception the exception
+     */
+    @Autowired
+    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("l").password("p").roles("USER");
+                .withUser("l").password("p").roles("ADMIN", "USER").and()
+                .withUser("ll").password("pp").roles("USER");
     }
+
+
+    @Override
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/oauth/token").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .realmName("CRM_REALM");
+    }
+
 
     @Override
     @Bean
-    public AuthenticationManager authenticationManagerBean()
-            throws Exception {
+    public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/login").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin().permitAll();
-//    }
-
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-
-        http
-                .authorizeRequests()
-                .antMatchers("/registration", "/error-registration").anonymous()
-                .antMatchers("/registration").permitAll()
-                .antMatchers("/**").hasRole("USER")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().permitAll()
-                .and()
-                .httpBasic();
+    /**
+     * Token store token store.
+     *
+     * @return the token store
+     */
+    @Bean
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
     }
+
+    /**
+     * User approval handler token store user approval handler.
+     *
+     * @param tokenStore the token store
+     * @return the token store user approval handler
+     */
+    @Bean
+    @Autowired
+    public TokenStoreUserApprovalHandler userApprovalHandler(TokenStore tokenStore) {
+        TokenStoreUserApprovalHandler handler = new TokenStoreUserApprovalHandler();
+        handler.setTokenStore(tokenStore);
+        handler.setRequestFactory(new DefaultOAuth2RequestFactory(clientDetailsService));
+        handler.setClientDetailsService(clientDetailsService);
+        return handler;
+    }
+
+    /**
+     * Approval store approval store.
+     *
+     * @param tokenStore the token store
+     * @return the approval store
+     * @throws Exception the exception
+     */
+    @Bean
+    @Autowired
+    public ApprovalStore approvalStore(TokenStore tokenStore) throws Exception {
+        TokenApprovalStore store = new TokenApprovalStore();
+        store.setTokenStore(tokenStore);
+        return store;
+    }
+
 }

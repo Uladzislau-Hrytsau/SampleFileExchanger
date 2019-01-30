0cc0e7fb4dd4ef4cd6e24829912d0d80a3b64907
vladikini45 <vladboy45@gmail.com>
2019-01-30 12:24:43 +0300

    fixed module front-end

--- front-end/vue-app/src/main.js
+++ front-end/vue-app/src/main.js
@@ -2,9 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import App from './App'
 import router from './router'
+import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 
 Vue.use(BootstrapVue);
 

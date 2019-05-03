cf46af2409b08aa71c9d6fb4fa2da48c10c29815
vladikini45 <vladboy45@gmail.com>
2019-05-03 20:04:39 +0300

    test commit

---
+++ service/src/main/java/com/exchange/configuration/security/ResourceServerOAuth2Configuration.java
@@ -0,0 +1,36 @@
+package com.exchange.configuration.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+/**
+ * The type Resource server o auth 2 configuration.
+ */
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ResourceServerOAuth2Configuration
+        extends ResourceServerConfigurerAdapter {
+
+    private static final String OAUTH_TOKEN_ENDPOINT = "/oauth/token";
+    private static final String USER_ENDPOINT = "/users";
+    private static final String ALL_ENDPOINTS = "/**";
+    private static final String HAS_ROLE_ADMIN_OR_HAS_ROLE_USER = "hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')";
+
+    @Override
+    public void configure(final HttpSecurity http) throws Exception {
+        http
+                .anonymous().and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, OAUTH_TOKEN_ENDPOINT).permitAll()
+                .antMatchers(HttpMethod.POST, OAUTH_TOKEN_ENDPOINT).permitAll()
+                .antMatchers(HttpMethod.OPTIONS, USER_ENDPOINT).permitAll()
+                .antMatchers(HttpMethod.POST, USER_ENDPOINT).permitAll()
+                .antMatchers(ALL_ENDPOINTS).access(HAS_ROLE_ADMIN_OR_HAS_ROLE_USER);
+    }
+}
---
+++ service/src/main/java/com/exchange/configuration/security/SecurityInitializerConfiguration.java
@@ -0,0 +1,12 @@
+package com.exchange.configuration.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * The type Security initializer configuration.
+ */
+@Configuration
+public class SecurityInitializerConfiguration extends AbstractSecurityWebApplicationInitializer {
+
+}
